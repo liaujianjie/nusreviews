a01a9a840556af372a44705b75401c6ef8127347
Eloise <limeloiseyy@gmail.com>
2019-09-12 23:59:10 +0800

    Added image

--- frontend/src/pages/seachmod.tsx
+++ frontend/src/pages/seachmod.tsx
@@ -1,27 +1,34 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import HeaderMenu from "../components/HeaderMenu/HeaderMenu";
-import { withLayout, LayoutProps, menuItems } from "../components/Layout";
-import {Grid,Header,Search} from "semantic-ui-react";
+import { withLayout } from "../components/Layout";
+import { Grid, Search, Message } from "semantic-ui-react";
+import logo from "../assets/logo.svg";
 
-
-
-const searchmodPage = () => {
-
+const SearchPage = () => {
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
-
       <Grid.Column>
-        <Header as="h1" color="teal" textAlign="center">
-          NUS Reviews
-        </Header>
-        
-          <Search color="blue" fluid size="massive" placeholder="Search"> 
-          <input class = "prompt" type ="text" placeholder="Search"/>
-          </Search>
-        
+        <img src={logo} alt="logo" style={{ minWidth: 450, padding: "4%" }} />
+
+        <div class="ui center aligned grid">
+          <div class="eight wide column">
+            <Search
+              color="blue"
+              fluid
+              size="massive"
+              placeholder="Search modules or lecturers"
+              input={{ fluid: true }}
+            >
+              <input type="text" />
+            </Search>
+          </div>
+        </div>
+
+        <Message style={{ marginBottom: "2em" }}>
+          To preserve the safe space of this community, we ask that you do not
+          personally identify or reveal any students from your reviews.
+        </Message>
       </Grid.Column>
     </Grid>
   );
 };
-export default withLayout(searchmodPage);
+export default withLayout(SearchPage);
