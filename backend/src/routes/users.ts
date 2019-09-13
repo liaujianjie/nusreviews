3162de565780e03633f839a8c95ee39e510e53ce
Shawn Koh <shawnkoh@me.com>
2019-09-13 19:20:06 +0800

    Call middlewares on routes

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,16 +1,35 @@
 import { UsersController } from "../controllers/UsersController";
 import { Router } from "express";
 import { checkJwt } from "../middlewares/checkJwt";
+import { checkRole } from "../middlewares/checkRole";
 
 export const router = Router();
 
+/**
+ * Sign in
+ *
+ * @returns jsonwebtoken
+ */
 router.post("/sign_in", UsersController.signIn);
+
 // router.delete("/sign_out", UsersController.signOut);
+
+// Reset password
 router.put("/password", [checkJwt], UsersController.resetPassword);
 
-router.get("/", UsersController.all);
-router.get("/:id", UsersController.one);
+// Request password reset
+// router.post("/password", UsersController.requestPasswordReset);
+
+// Get all users
+router.get("/", [checkJwt, checkRole(["ADMIN"])], UsersController.all);
+
+// Get a user
+router.get("/:id", [checkJwt, checkRole(["ADMIN"])], UsersController.one);
+
+// Create a new user
 router.post("/", UsersController.save);
-router.delete("/", UsersController.remove);
+
+// Soft-delete a user
+router.delete("/", [checkJwt, checkRole(["ADMIN"])], UsersController.remove);
 
 export default router;
