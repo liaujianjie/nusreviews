5a69faaee1a161fcae93cdede35e44f8d317c191
Eloise <limeloiseyy@gmail.com>
2019-09-14 23:45:56 +0800

    added rating

--- frontend/src/pages/modlist.tsx
+++ frontend/src/pages/modlist.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
-import { Card, Container, Rating, Icon } from "semantic-ui-react";
+import {
+  Card,
+  Container,
+  Rating,
+  Header
+} from "semantic-ui-react";
 import { withLayout } from "../components/Layout";
-import ActionButton from "../components/ActionButton";
-import Section from "../components/Section";
 import RatingCard from "../components/RatingCard";
+import { ratings } from "./module";
+import Section from "../components/Section";
 
 const Details = () => {
   const ModuleOptions = [
@@ -11,7 +16,7 @@ const Details = () => {
       key: "1920sem1",
       code: "CS3216",
       name: "Software Product Engineering for Digital Markets",
-      value: 3,
+      value: 5,
       description:
         "In this module, students will practice software product engineering by working in small teams to develop well-tested, user-friendly, production-quality software for the real world. To support this goal, students work closely with users to understand their problems, gather their requirements, and obtain their feedback through a rapid, iterative, application design and development process. Students will also be exposed to practical issues for digital markets such as growing the user base of their application, deployment of the application on the Web or in the cloud system, and validating the UI design and UX of the application."
     },
@@ -25,13 +30,38 @@ const Details = () => {
   ];
 
   return ModuleOptions.map(mod => (
-    <Card.Group >
+    <Card.Group>
       <Card color="orange" fluid>
         <Card.Content extra>
-        <h1>{mod.code + " " + mod.name}</h1>
-        Overall Rating:<Rating icon="star" defaultRating={mod.value} maxRating={5} />
+          <Section
+            topLeft={
+              <a href="../module">
+                <Header as="h2">
+                  {mod.code}
+                  <Header.Subheader style={{ color: "black" }}>
+                    {mod.name}
+                  </Header.Subheader>
+                </Header>
+              </a>
+            }
+            topRight={
+              <b style={{ color: "black" }}>
+                Overall Rating:
+                <Rating icon="star" defaultRating={mod.value} maxRating={5} />
+              </b>
+            }
+            body={
+              <div>
+                <p style={{ color: "black" }}>
+                  {mod.description}
+                  <br />
+                  <br />
+                  <RatingCard ratings={ratings} />
+                </p>
+              </div>
+            }
+          />
         </Card.Content>
-        <Card.Content description={mod.description} />
       </Card>
     </Card.Group>
   ));
@@ -39,15 +69,13 @@ const Details = () => {
 
 const ModuleList = () => {
   return (
-    
     <div>
-     
       <Container
         style={{
           marginTop: "6rem"
         }}
       >
-         <h1>Search Results for CS321:</h1>
+        <h1>Search Results for CS321:</h1>
         <Details />
       </Container>
     </div>
