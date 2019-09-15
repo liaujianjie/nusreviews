5a10b37239d2ad7a51909f938e6405760cf6b0e5
Branson <branson.ng@u.nus.edu>
2019-09-15 14:26:56 +0800

    Add Module header Add RadioButton Group

--- frontend/src/pages/form.tsx
+++ frontend/src/pages/form.tsx
@@ -1,44 +1,73 @@
 import * as React from "react";
-import { range } from "lodash";
 // external component libraries
 import * as Final from "react-final-form";
-import { Form, Container, Header, Grid, Label } from "semantic-ui-react";
+import { Form, Header, Segment, Grid } from "semantic-ui-react";
 // components
 import { withLayout } from "../components/Layout";
-import RadioButton from "../components/Form/RadioButton";
+import RadioButtonGroup from "../components/Form/RadioButton";
 import LongTextInput from "../components/Form/LongTextInput";
 import ShortTextInput from "../components/Form/ShortTextInput";
 import DropDown from "../components/Form/DropDown";
 // interfaces
 import { LongFormReviewInput } from "../components/Form/Form";
 // constants
-import { grades } from "../constants/Form";
+import {
+  grades,
+  lecturerQuestions,
+  workloadQuestions
+} from "../constants/Form";
+
+const ModuleDetail = () => {
+  return (
+    <React.Fragment>
+      <Header as="h2" style={{ display: "inline" }}>
+        CS3216
+        <Header.Subheader
+          style={{
+            display: "inline",
+            color: "#747474",
+            background: "#F2F2F2",
+            borderRadius: "8px",
+            paddingLeft: "4px"
+          }}
+        >
+          AY2019/2020, SEM1
+        </Header.Subheader>
+      </Header>
+
+      <Grid columns="equal">
+        <Grid.Column floated="left">
+          Software Product Engineering for Digital Markets
+        </Grid.Column>
+        <Grid.Column floated="right" textAlign="right">
+          Do as you wish, there are no compulsory fields
+        </Grid.Column>
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 const LongForm = () => {
   const onSubmit = (values: LongFormReviewInput) => {
     window.alert(JSON.stringify(values, 0, 2));
   };
 
-  const getRadioButtonGroup = (scale: number, field: string) => {
-    return range(scale).map(num => (
-      <Grid.Column>
-        <RadioButton value={num} name={field} />
-      </Grid.Column>
-    ));
+  const getRadioButtons = (questionSet: Array<any>) => {
+    return questionSet.map(values => <RadioButtonGroup {...values} />);
   };
 
   return (
-    <Container textAlign="center" text fluid>
-      <Header as="h2">CS3216 AY2019/2020 SEM 1</Header>
-      <Final.Form onSubmit={onSubmit}>
-        {({ handleSubmit, form, values }) => (
-          <Form
-            onSubmit={e => {
-              handleSubmit(e);
-              form.reset();
-            }}
-          >
-            <Form.Group widths="equal" style={{ justifyContent: "center" }}>
+    <Final.Form onSubmit={onSubmit}>
+      {({ handleSubmit, form, values }) => (
+        <Form
+          onSubmit={e => {
+            handleSubmit(e);
+            form.reset();
+          }}
+        >
+          <ModuleDetail />
+          <Segment clearing basic padded>
+            <Form.Group widths="equal">
               <DropDown
                 name="expectedGrade"
                 placeholder={grades[0].text}
@@ -56,44 +85,36 @@ const LongForm = () => {
                 Actual Grade
               </DropDown>
             </Form.Group>
+          </Segment>
 
-            <Form.Group inline style={{ justifyContent: "center" }}>
-              <Header as="h5">How was the Lecturer?</Header>
-              <Label basic>Avoid</Label>
-              {getRadioButtonGroup(5, "Lecturer")}
-              <Label basic>Recommend</Label>
-            </Form.Group>
+          {getRadioButtons(lecturerQuestions)}
+          {getRadioButtons(workloadQuestions)}
 
-            <Form.Group>
-              <ShortTextInput
-                name="name"
-                placeholder="Name"
-                value={values.name}
-              >
-                Name
-              </ShortTextInput>
-              <ShortTextInput
-                name="realName"
-                placeholder="RealName"
-                value={values.realName}
-              >
-                Real Name
-              </ShortTextInput>
-            </Form.Group>
-
-            <LongTextInput
-              name="description"
-              placeholder="The world was gonna roll me"
-              value={values.description}
+          <Form.Group>
+            <ShortTextInput name="name" placeholder="Name" value={values.name}>
+              Name
+            </ShortTextInput>
+            <ShortTextInput
+              name="realName"
+              placeholder="RealName"
+              value={values.realName}
             >
-              SOMEBODY ONCE TOLD ME
-            </LongTextInput>
+              Real Name
+            </ShortTextInput>
+          </Form.Group>
 
-            <Form.Button type="submit">Submit</Form.Button>
-          </Form>
-        )}
-      </Final.Form>
-    </Container>
+          <LongTextInput
+            name="description"
+            placeholder="The world was gonna roll me"
+            value={values.description}
+          >
+            SOMEBODY ONCE TOLD ME
+          </LongTextInput>
+
+          <Form.Button type="submit">Submit</Form.Button>
+        </Form>
+      )}
+    </Final.Form>
   );
 };
 
