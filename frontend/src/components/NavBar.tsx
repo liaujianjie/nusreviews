4beb41ea41e7c3a2b9b9a67b9ad34790be2b0d05
elmoose <52392321+elmoose@users.noreply.github.com>
2019-09-15 10:45:45 +0800

    Add search page (#3)
    * added search mod page
    
    * Added image
    
    * removed message
    
    * .DS_Store removed
    
    * added a page to view modules
    
    * added overall rating
    
    * added rating
    
    * changed the typo
    
    * redirects to searchpage when logo is pressed

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,29 +1,23 @@
 import * as React from "react";
 import { Menu, Search, Image, Container } from "semantic-ui-react";
-import logo from '../static/images/logo.svg'
+import logo from "../static/images/logo.svg";
 
 const NavBar = () => {
   return (
-    <Menu 
-      fixed='top' 
-      verticalAlign="middle"
-      borderless
-    >
+    <Menu fixed="top" verticalAlign="middle" borderless>
       <Container>
         <Menu.Item>
-          <Image src={logo} alt="logo" size="small"/>
+          <a href="../search">
+            <Image src={logo} alt="logo" size="small" />
+          </a>
         </Menu.Item>
-        <Menu.Menu position='right'>
+        <Menu.Menu position="right">
           <Menu.Item>
-            <Search
-              size="mini"
-              fluid
-              placeholder="Search a module"
-            />
+            <Search size="mini" fluid placeholder="Search a module" />
           </Menu.Item>
           <Menu.Item>
-            <Image 
-              src='https://66.media.tumblr.com/d4743332c9f25d147aac03ec12b9c9a2/tumblr_ory27uhmTl1w84hv9o6_250.jpg'
+            <Image
+              src="https://66.media.tumblr.com/d4743332c9f25d147aac03ec12b9c9a2/tumblr_ory27uhmTl1w84hv9o6_250.jpg"
               avatar
             />
             <span>Jennie Kim</span>
@@ -31,7 +25,7 @@ const NavBar = () => {
         </Menu.Menu>
       </Container>
     </Menu>
-  )
+  );
 };
 
 export default NavBar;
