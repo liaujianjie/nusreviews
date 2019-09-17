654951849873594e70b8650af9e784b2981c88ac
Shawn Koh <shawnkoh@me.com>
2019-09-17 19:29:29 +0800

    Set up .env

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -2,11 +2,12 @@ import { ConnectionOptions } from "typeorm";
 
 export const postgres: ConnectionOptions = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
+  database: process.env.DB_NAME,
+  ssl: true,
   synchronize: true,
   logging: false,
   entities: ["src/entities/**/*.ts"],
