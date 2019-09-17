e062c2e721162f1e72b4c806581a12343cf25660
Shawn Koh <shawnkoh@me.com>
2019-09-17 19:30:44 +0800

    Implement more class validators for Module

--- backend/src/entities/Module.ts
+++ backend/src/entities/Module.ts
@@ -8,6 +8,7 @@ import {
   Workload,
   NUSModuleAttributes
 } from "../types/modules";
+import { IsArray, IsOptional, IsNotEmpty, IsString } from "class-validator";
 
 @Entity()
 export class Module extends Discardable {
@@ -19,37 +20,50 @@ export class Module extends Discardable {
     type: "varchar",
     unique: true
   })
+  @IsNotEmpty()
   moduleCode!: ModuleCode;
 
   @Column({
     type: "varchar"
   })
+  @IsNotEmpty()
+  @IsString()
   title!: ModuleTitle;
 
   // Additional info
   @Column({
     nullable: true
   })
+  @IsOptional()
+  @IsString()
   description?: string;
 
   @Column()
+  @IsNotEmpty()
+  @IsString()
   moduleCredit!: string;
 
   @Column({
     type: "varchar"
   })
+  @IsNotEmpty()
+  @IsString()
   department!: Department;
 
   @Column({
     type: "varchar"
   })
+  @IsNotEmpty()
+  @IsString()
   faculty!: Faculty;
 
   @Column({
-    type: "int",
+    type: "real",
     array: true,
     nullable: true
   })
+  @IsOptional()
+  @IsArray()
   workload?: Workload;
 
   @Column({
@@ -57,22 +71,30 @@ export class Module extends Discardable {
     array: true,
     nullable: true
   })
+  @IsOptional()
+  @IsArray()
   attributes?: NUSModuleAttributes;
 
   // Requisites
   @Column({
     nullable: true
   })
+  @IsOptional()
+  @IsString()
   prerequisite?: string;
 
   @Column({
     nullable: true
   })
+  @IsOptional()
+  @IsString()
   corequisite?: string;
 
   @Column({
     nullable: true
   })
+  @IsOptional()
+  @IsString()
   preclusion?: string;
 
   // Semester data is stored in semester and module_semester
