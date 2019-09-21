703e8233c1e5b44ec0b94839633b42701b37eb3d
Shawn Koh <shawnkoh@me.com>
2019-09-21 20:36:21 +0800

    Add MetricTemplate properties

--- backend/src/entities/MetricTemplate.ts
+++ backend/src/entities/MetricTemplate.ts
@@ -1,4 +1,5 @@
-import { Entity, OneToMany, ManyToOne } from "typeorm";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Entity, OneToMany, ManyToOne, Column } from "typeorm";
 import { Discardable } from "./Discardable";
 import { Metric } from "./Metric";
 import { ReviewTemplate } from "./ReviewTemplate";
@@ -10,6 +11,30 @@ export class MetricTemplate extends Discardable {
     reviewTemplate => reviewTemplate.metricTemplates
   )
   reviewTemplate!: ReviewTemplate;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsNumber()
+  minValue!: number;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  minDescription!: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsNumber()
+  maxValue!: number;
+
+  @Column()
+  @IsNotEmpty()
+  maxDescription!: string;
 
   @OneToMany(type => Metric, metric => metric.metricTemplate)
   metrics!: Metric[];
