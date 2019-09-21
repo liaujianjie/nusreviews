da1b8994a973f950fedef263e7c2c75839b1bfd0
Shawn Koh <shawnkoh@me.com>
2019-09-21 21:26:27 +0800

    Change grade enums to be unique

--- backend/src/types/reviews.ts
+++ backend/src/types/reviews.ts
@@ -1,15 +1,15 @@
 export enum Grade {
-  Aplus = 5,
-  A = 5,
-  Aminus = 4.5,
-  Bplus = 4,
-  B = 3.5,
-  Bminus = 3,
-  Cplus = 2.5,
-  C = 2,
-  Dplus = 1.5,
-  D = 1,
-  F = 0,
-  S = 0,
-  U = 0
+  Aplus,
+  A,
+  Aminus,
+  Bplus,
+  B,
+  Bminus,
+  Cplus,
+  C,
+  Dplus,
+  D,
+  F,
+  S,
+  U
 }
