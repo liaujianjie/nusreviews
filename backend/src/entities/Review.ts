8360451603d96ae96249372e658a9bada6801e91
Shawn Koh <shawnkoh@me.com>
2019-09-21 20:29:02 +0800

    Add ModuleSemester has_many Review

--- backend/src/entities/Review.ts
+++ backend/src/entities/Review.ts
@@ -1,13 +1,37 @@
-import { Entity, OneToMany, ManyToOne } from "typeorm";
+import { Entity, OneToMany, ManyToOne, Column } from "typeorm";
 import { Discardable } from "./Discardable";
 import { Metric } from "./Metric";
 import { Question } from "./Question";
 import { ReviewTemplate } from "./ReviewTemplate";
+import { ModuleSemester } from "./ModuleSemester";
+import { IsOptional, IsEnum } from "class-validator";
+import { Grade } from "../types/reviews";
 
 @Entity()
 export class Review extends Discardable {
   @ManyToOne(type => ReviewTemplate, reviewTemplate => reviewTemplate.reviews)
   reviewTemplate!: ReviewTemplate;
+
+  @ManyToOne(type => ModuleSemester, moduleSemester => moduleSemester.reviews)
+  moduleSemester!: ModuleSemester;
+
+  @Column({
+    type: "enum",
+    nullable: true,
+    enum: Grade
+  })
+  @IsOptional()
+  @IsEnum(Grade)
+  expectedGrade?: Grade;
+
+  @Column({
+    type: "enum",
+    nullable: true,
+    enum: Grade
+  })
+  @IsOptional()
+  @IsEnum(Grade)
+  actualGrade?: Grade;
 
   @OneToMany(type => Metric, metric => metric.review)
   metrics!: Metric[];
---
+++ backend/src/types/reviews.ts
@@ -0,0 +1,15 @@
+export enum Grade {
+  Aplus = 5,
+  A = 4.5,
+  Aminus = 4,
+  Bplus = 3.5,
+  B = 3,
+  Bminus = 2.5,
+  Cplus = 2,
+  C = 1.5,
+  Dplus = 1.5,
+  D = 1,
+  F = 0,
+  S = 0,
+  U = 0
+}
