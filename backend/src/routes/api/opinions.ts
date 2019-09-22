dc8ea8ea7c33a84e99d50fd3c87ca1ecb0d2f6a3
Shawn Koh <shawnkoh@me.com>
2019-09-22 22:11:40 +0800

    DELETE /opinions/:id, PATCH /opinions/:id/undiscard, DELETE /tips/:id, PATCH /tips/:id/undiscard

--- backend/src/routes/api/opinions.ts
+++ backend/src/routes/api/opinions.ts
@@ -3,6 +3,8 @@ import * as OpinionsController from "../../controllers/OpinionsController";
 import * as OpinionVotesController from "../../controllers/OpinionVotesController";
 import { checkEditToken } from "../../middlewares/checkEditToken";
 import { checkAccessToken } from "../../middlewares/checkAccessToken";
+import { checkRole } from "../../middlewares/checkRole";
+import { UserRole } from "../../types/users";
 
 export const router = Router();
 
@@ -12,5 +14,8 @@ router.get("/:id/votes", OpinionsController.votes);
 
 router.use(checkAccessToken);
 router.post("/:id/votes", OpinionVotesController.create);
+router.use(checkRole([UserRole.Admin]));
+router.delete("/:id", OpinionsController.discard);
+router.patch("/:id/undiscard", OpinionsController.undiscard);
 
 export default router;
--- backend/src/routes/api/tips.ts
+++ backend/src/routes/api/tips.ts
@@ -3,6 +3,8 @@ import * as TipsController from "../../controllers/TipsController";
 import * as TipVotesController from "../../controllers/TipVotesController";
 import { checkEditToken } from "../../middlewares/checkEditToken";
 import { checkAccessToken } from "../../middlewares/checkAccessToken";
+import { checkRole } from "../../middlewares/checkRole";
+import { UserRole } from "../../types/users";
 
 export const router = Router();
 
@@ -12,5 +14,8 @@ router.get("/:id/votes", TipsController.votes);
 
 router.use(checkAccessToken);
 router.post("/:id/votes", TipVotesController.create);
+router.use(checkRole([UserRole.Admin]));
+router.delete("/:id", TipsController.discard);
+router.patch("/:id/undiscard", TipsController.undiscard);
 
 export default router;
