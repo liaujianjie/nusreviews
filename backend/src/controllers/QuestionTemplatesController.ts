1a3b316f676f1a7ac692a96187ffc4b0555e8f10
Shawn Koh <shawnkoh@me.com>
2019-09-22 19:55:13 +0800

    Remove MetricTemplatesController and QuestionTemplatesController

--- backend/src/controllers/QuestionTemplatesController.ts
+++
@@ -1,5 +0,0 @@
-import { Response, Request } from "express";
-
-export async function index(request: Request, response: Response) {}
-
-export async function show(request: Request, response: Response) {}
