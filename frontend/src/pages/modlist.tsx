419c1e3985bef813d68234e80294512b2f750d48
Eloise <limeloiseyy@gmail.com>
2019-09-24 12:01:25 +0800

    Add highlight

--- frontend/src/pages/modlist.tsx
+++ frontend/src/pages/modlist.tsx
@@ -1,35 +1,16 @@
 import * as React from "react";
-
-import { Card, Container, Rating, Header, Button } from "semantic-ui-react";
+import { Card, Container, Header } from "semantic-ui-react";
 import { withLayout } from "../components/Layout";
 import RatingCard from "../components/RatingCard";
 import { ratings } from "./module";
 import Section from "../components/Section";
-import { connectInfiniteHits } from "react-instantsearch-dom";
+import { Highlight, InfiniteHits, Configure } from "react-instantsearch-dom";
 import { CurrentRefinements } from "react-instantsearch-dom";
 import { InstantSearch } from "react-instantsearch-dom";
 import { withAuth } from "../components/withAuth";
 
-const Details = hits => {
-  const ModuleOptions = [
-    {
-      key: "1920sem1",
-      code: "CS3216",
-      name: "Software Product Engineering for Digital Markets",
-      value: 5,
-      description:
-        "In this module, students will practice software product engineering by working in small teams to develop well-tested, user-friendly, production-quality software for the real world. To support this goal, students work closely with users to understand their problems, gather their requirements, and obtain their feedback through a rapid, iterative, application design and development process. Students will also be exposed to practical issues for digital markets such as growing the user base of their application, deployment of the application on the Web or in the cloud system, and validating the UI design and UX of the application."
-    },
-    {
-      key: "Sem1",
-      code: "CS3210",
-      name: "I'm a stupid mod",
-      value: 3,
-      description: "Ahhhhhh"
-    }
-  ];
-
-  return hits.map(hit => (
+function Hit(props) {
+  return (
     <Card.Group>
       <Card color="orange" fluid>
         <Card.Content extra>
@@ -37,23 +18,17 @@ const Details = hits => {
             topLeft={
               <a href="../module">
                 <Header as="h2">
-                  {hit.moduleCode}
+                  <Highlight attribute="moduleCode" hit={props.hit} />
                   <Header.Subheader style={{ color: "black" }}>
-                    {hit.title}
+                    <Highlight attribute="title" hit={props.hit} />
                   </Header.Subheader>
                 </Header>
               </a>
             }
-            topRight={
-              <b style={{ color: "black" }}>
-                Overall Rating:
-                <Rating icon="star" defaultRating={0} maxRating={5} disabled />
-              </b>
-            }
             body={
               <div>
                 <p style={{ color: "black" }}>
-                  {hit.description}
+                  <Highlight attribute="description" hit={props.hit} />
                   <br />
                   <br />
                   <RatingCard ratings={ratings} />
@@ -64,10 +39,10 @@ const Details = hits => {
         </Card.Content>
       </Card>
     </Card.Group>
-  ));
-};
+  );
+}
 
-const ModuleList = (hasMore, refineNext) => {
+const ModuleList = () => {
   return (
     <div>
       <Container
@@ -82,18 +57,12 @@ const ModuleList = (hasMore, refineNext) => {
           appId="092aa257d26c6e1fb8733a3c0229b176"
           indexName="modules"
         >
-          <header className="header">
-            <CustomInfiniteHits />
-            <Button disabled={!hasMore} onClick={refineNext}>
-              Show more
-            </Button>
-          </header>
+          <Configure hitsPerPage={10} />
+          <InfiniteHits hitComponent={Hit} />
         </InstantSearch>
       </Container>
     </div>
   );
 };
 
-
-const CustomInfiniteHits = connectInfiniteHits(Details);
 export default withLayout(ModuleList);
