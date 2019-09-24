73b693cd6d05adca79c16d8d5258be6bfd2a57a4
Branson Ng <41634392+BransonNg@users.noreply.github.com>
2019-09-24 15:39:57 +0800

    Short form (#33)
    * Add ShortReview modal

--- frontend/src/components/ActionButton.tsx
+++ frontend/src/components/ActionButton.tsx
@@ -2,23 +2,28 @@ import * as React from "react";
 import { Icon, Button } from "semantic-ui-react";
 
 type Props = {
-  icon: string,
-  name: string,
-  transparent: boolean,
-}
+  name: string;
+  transparent: boolean;
+  onClick: Function;
+  icon?: string;
+  disabled?: boolean;
+};
 
 const ActionButton = (props: Props) => {
-  const { icon, name, transparent } = props;
+  const { icon, name, transparent, onClick, disabled } = props;
   return (
-    <Button 
+    <Button
+      onClick={() => onClick()}
       style={{
-        backgroundColor: (transparent ? 'transparent' : '#fc4838'),
-        color: (transparent ? '#fc4838' : '#fff'),
-        borderRadius: "0.3em",
-      }}>
-      {icon && <Icon name={icon}/>} {name}
+        backgroundColor: transparent ? "transparent" : "#fc4838",
+        color: transparent ? "#fc4838" : "#fff",
+        borderRadius: "0.3em"
+      }}
+      disabled={disabled}
+    >
+      {icon && <Icon name={icon} />} {name}
     </Button>
-  )
+  );
 };
 
 export default ActionButton;
