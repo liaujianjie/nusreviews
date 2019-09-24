ccc944c363773b09a0157d31c257119344852d76
Shawn Koh <shawnkoh@me.com>
2019-09-25 03:17:07 +0800

    Remove GET /modules and /modules/:id/semesters

--- backend/src/controllers/ModulesController.ts
+++ backend/src/controllers/ModulesController.ts
@@ -4,21 +4,6 @@ import { Module } from "../entities/Module";
 import { Semester } from "../entities/Semester";
 import { MetricTemplate } from "../entities/MetricTemplate";
 
-export async function index(
-  request: Request,
-  response: Response,
-  next: NextFunction
-) {
-  let result;
-  try {
-    result = await getRepository(Module).find();
-    // TODO: need to cut this down to send a more condensed version
-    response.status(200).json(result);
-  } catch (error) {
-    response.sendStatus(400);
-  }
-}
-
 export async function show(
   request: Request,
   response: Response,
@@ -60,27 +45,6 @@ export async function show(
     });
     const result = { ...module, metricAggregates, metricTemplates };
     response.status(200).json(result);
-  } catch (error) {
-    response.sendStatus(400);
-  }
-}
-
-export async function semesters(
-  request: Request,
-  response: Response,
-  next: NextFunction
-) {
-  try {
-    const module = await getRepository(Module).findOneOrFail({
-      where: { moduleCode: request.params.module_code },
-      relations: ["moduleSemesters", "moduleSemesters.semester"]
-    });
-    const moduleSemesters = module.moduleSemesters;
-    const semesters: Semester[] = [];
-    moduleSemesters.forEach(moduleSemester => {
-      semesters.push(moduleSemester.semester);
-    });
-    response.status(200).json(semesters);
   } catch (error) {
     response.sendStatus(400);
   }
--- backend/src/routes/api/modules.ts
+++ backend/src/routes/api/modules.ts
@@ -3,9 +3,7 @@ import * as ModulesController from "../../controllers/ModulesController";
 
 export const router = Router();
 
-router.get("/", ModulesController.index);
 router.get("/:module_code", ModulesController.show);
-router.get("/:module_code/semesters", ModulesController.semesters);
 router.get("/:module_code/module_semesters", ModulesController.moduleSemesters);
 
 export default router;
