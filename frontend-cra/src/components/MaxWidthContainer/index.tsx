dc203f005ec05364e7f96998edd58b6170380c9e
Liau Jian Jie <liaujianjie@gmail.com>
2019-09-25 16:50:24 +0800

    Apply bootstrap responsive layout to root containers

---
+++ frontend-cra/src/components/MaxWidthContainer/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./style.css";
+
+export const MaxWidthContainer: React.FunctionComponent = ({ children }) => {
+  return <div className="MaxWidthContainer">{children}</div>;
+};
