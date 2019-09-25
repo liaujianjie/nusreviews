0f9f35a93b799190d4feab047f08831f1fe9c86f
Liau Jian Jie <liaujianjie@gmail.com>
2019-09-25 21:58:29 +0800

    Move backend base url to env config (#47)

--- frontend-cra/src/api/sharedHttpClient.ts
+++ frontend-cra/src/api/sharedHttpClient.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+/**
+ * React environment variables must be pefixed with `REACT_APP`.
+ *
+ * See: https://create-react-app.dev/docs/adding-custom-environment-variables
+ */
+const { REACT_APP_BACKEND_BASE_URL } = process.env;
+
+if (!REACT_APP_BACKEND_BASE_URL) {
+  throw new Error(
+    "REACT_APP_BACKEND_BASE_URL environment variable not provided!"
+  );
+}
+
 export const sharedHttpClient = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: REACT_APP_BACKEND_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/x-www-form-urlencoded"
