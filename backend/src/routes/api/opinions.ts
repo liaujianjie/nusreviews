d3c7f303b0cd15903d38babab12412e011a6984e
Shawn Koh <shawnkoh@me.com>
2019-09-26 21:17:08 +0800

    Add emailVerified to authentication tokens and emailVerified checks for creating tips, opinions, reviews, tip votes and opinion votes

--- backend/src/routes/api/opinions.ts
+++ backend/src/routes/api/opinions.ts
@@ -5,6 +5,7 @@ import { checkBearerToken } from "../../middlewares/checkBearerToken";
 import { checkRole } from "../../middlewares/checkRole";
 import { UserRole } from "../../types/users";
 import { BearerTokenType } from "../../types/tokens";
+import { checkEmailVerified } from "../../middlewares/checkEmailVerified";
 
 export const router = Router();
 
@@ -12,6 +13,7 @@ router.get("/:id", OpinionsController.show);
 router.get("/:id/votes", OpinionsController.votes);
 
 router.use(checkBearerToken(BearerTokenType.AccessToken));
+router.use(checkEmailVerified);
 router.post("/:id/votes", OpinionVotesController.create);
 router.use(checkRole([UserRole.Admin]));
 router.delete("/:id", OpinionsController.discard);
