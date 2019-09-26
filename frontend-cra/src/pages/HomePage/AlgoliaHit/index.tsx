e322e268840b5822ac92fa44b8add4cf4b987eb7
Liau Jian Jie <liaujianjie@gmail.com>
2019-09-26 18:50:11 +0800

    Fix incorrect color for anchor tags

--- frontend-cra/src/pages/HomePage/AlgoliaHit/index.tsx
+++ frontend-cra/src/pages/HomePage/AlgoliaHit/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { BasicDoc } from "react-instantsearch-core";
 import { MenuItem } from "@blueprintjs/core";
+import { Link } from "react-router-dom";
+
+import "./style.css";
 
 export const AlgoliaHit: React.FunctionComponent<{ hit: BasicDoc }> = ({
   hit
 }) => {
   return (
     <MenuItem
+      className="AlgoliaHit"
       text={
-        <div>
+        <Link to={`/module/${hit.moduleCode}`}>
           <div>
-            <strong>{hit.moduleCode}</strong> <span>{hit.title}</span>
+            <div>
+              <strong>{hit.moduleCode}</strong> <span>{hit.title}</span>
+            </div>
+            <div className="bp3-text-muted">{hit.department}</div>
           </div>
-          <div className="bp3-text-muted">{hit.department}</div>
-        </div>
+        </Link>
       }
     />
   );
