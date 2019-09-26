d79ab462ee475fe64a42bb5705701e05fc6a019a
Liau Jian Jie <liaujianjie@gmail.com>
2019-09-26 21:14:40 +0800

    Implement search UI (#63)
    * Move custom algolia components out of <HomePage> file
    
    * Fix incorrect color for anchor tags
    
    * Make it look snazzy

--- frontend-cra/src/pages/HomePage/index.tsx
+++ frontend-cra/src/pages/HomePage/index.tsx
@@ -1,19 +1,14 @@
-import React, { ChangeEvent } from "react";
+import React from "react";
 import algoliaSearch from "algoliasearch";
-import {
-  InstantSearch,
-  Configure,
-  connectSearchBox,
-  connectHits
-} from "react-instantsearch-dom";
-import { InputGroup, MenuItem, Menu, Card } from "@blueprintjs/core";
-import _ from "lodash";
+import { InstantSearch, Configure } from "react-instantsearch-dom";
 
 import { RequiresAuth } from "../../components/RequiresAuth";
-import { Center } from "../../components/Center";
 
+import { AlgoliaSearchBox } from "./AlgoliaSearchBox";
+
+import logo from "./logo.svg";
 import "./style.css";
-import { BasicDoc } from "react-instantsearch-core";
+import { Center } from "../../components/Center";
 
 const {
   REACT_APP_ALGOLIA_APPLICATION_ID,
@@ -29,75 +24,26 @@ const algoliaClient = algoliaSearch(
   REACT_APP_ALGOLIA_SEARCH_KEY
 );
 
-const AlgoliaSearchBox = connectSearchBox(
-  ({ refine, currentRefinement, isSearchStalled }) => (
-    <InputGroup
-      autoFocus
-      type="search"
-      leftIcon="search"
-      placeholder="Search for a module or lecturer..."
-      large
-      onChange={(event: ChangeEvent<HTMLInputElement>) =>
-        refine(event.target.value)
-      }
-      value={currentRefinement}
-    />
-  )
-);
-
-const AlgoliaHits = connectHits(({ hits }) => {
-  return (
-    <Card style={{ padding: 4 }}>
-      <Menu>
-        {_.map(hits).map((hit, index) => {
-          return (
-            <>
-              <AlgoliaHit hit={hit} />
-              {/* {hits.length - 1 !== index && <Divider />} */}
-            </>
-          );
-        })}
-      </Menu>
-    </Card>
-  );
-});
-
-const AlgoliaHit: React.FunctionComponent<{ hit: BasicDoc }> = ({ hit }) => {
-  return (
-    // <div style={{ padding: 8 }}>
-    <MenuItem
-      text={
-        <div>
-          <div>
-            <strong>{hit.moduleCode}</strong> <span>{hit.title}</span>
-          </div>
-          <div className="bp3-text-muted">{hit.department}</div>
-        </div>
-      }
-    />
-    // </div>
-  );
-};
-
 export const HomePage: React.FunctionComponent = () => {
   return (
     <RequiresAuth>
-      <Center>
-        <div className="HomePage__searchbar-container">
-          <InstantSearch indexName="modules" searchClient={algoliaClient}>
-            <Configure hitsPerPage={8} />
-            <AlgoliaSearchBox />
-            <br />
-            <AlgoliaHits />
-          </InstantSearch>
-          {/* <InputGroup
+      <div className="HomePage__searchbar-container">
+        <InstantSearch indexName="modules" searchClient={algoliaClient}>
+          <Configure hitsPerPage={8} />
+          <Center>
+            <div className="HomePage__logo-container">
+              <img className="HomePage__logo" src={logo} alt="header" />
+            </div>
+          </Center>
+          <AlgoliaSearchBox />
+        </InstantSearch>
+        {/* <InputGroup
             type="search"
             leftIcon="search"
             placeholder="Search for a module or lecturer..."
             large
           /> */}
-        </div>
-      </Center>
+      </div>
     </RequiresAuth>
   );
 };
