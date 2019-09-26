6bb8f77eb2073a80f240832b7711ee85f300e2d8
Liau Jian Jie <liaujianjie@gmail.com>
2019-09-27 01:22:36 +0800

    Implement verify email page (#70)
    * Add page for verifying email
    
    * Add button to reverify in case verification fails on first try

---
+++ frontend-cra/src/pages/VerifyEmailPage/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Card, Icon } from "@blueprintjs/core";
+
+import { Center } from "../../components/Center";
+import { AuthCardContainer } from "../../components/AuthCardContainer";
+import { VerifyEmailForm } from "./VerifyEmailForm";
+import { useRouter } from "../../hooks/useRouter";
+
+import "./style.css";
+
+export const VerifyEmailPage: React.FunctionComponent = () => {
+  const router = useRouter();
+
+  return (
+    <Center>
+      <AuthCardContainer>
+        <Card interactive onClick={() => router.history.push("/")}>
+          <Icon icon="arrow-left" /> <span>Go back</span>
+        </Card>
+        <Card>
+          <div className="VerifyEmailRequestPage__logo-container">
+            <Center>
+              <h1>Verify email</h1>
+            </Center>
+          </div>
+          <div className="VerifyEmailRequestPage__form-container">
+            <VerifyEmailForm />
+          </div>
+        </Card>
+      </AuthCardContainer>
+    </Center>
+  );
+};
