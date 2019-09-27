159e4adcf628d8e7b9c3767da2b9c1bdae9c5220
Branson <branson.ng@u.nus.edu>
2019-09-27 20:01:38 +0800

    Fix RadioButtonGroup display bug

--- frontend-cra/src/components/RadioButtonGroup/index.tsx
+++ frontend-cra/src/components/RadioButtonGroup/index.tsx
@@ -21,16 +21,16 @@ export const RadioButtonGroup: React.FunctionComponent<
   RadioGroupProps
 > = props => {
   const { minValue, maxValue, minDescription, maxDescription, name } = props;
-  const [width, setWidth] = React.useState(window.innerWidth);
-  React.useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  // const [width, setWidth] = React.useState(window.innerWidth);
+  // React.useEffect(() => {
+  //   const handleResize = () => {
+  //     setWidth(window.innerWidth);
+  //   };
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   const getRadioButtons = (mobile: boolean) => {
     return _.range(minValue, maxValue + 1).map(value => {
@@ -63,7 +63,7 @@ export const RadioButtonGroup: React.FunctionComponent<
         <div className={`RadioButtonGroup__left-segment-desktop`}>{name}</div>
         <div className={`RadioButtonGroup__right-segment-desktop`}>
           <label>{minDescription}</label>
-          {getRadioButtons(width < 800)}
+          {getRadioButtons(props.mobile)}
           <label>{maxDescription}</label>
         </div>
       </div>
